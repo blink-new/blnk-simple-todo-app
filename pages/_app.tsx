b748f0d70c4d07e8abe591dd7efc1f507884f6e3
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-02-24 06:29:14 +0000

    Transform into Professional Todo App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,11 @@
-import "@/styles/globals.css";
-import posthog from "posthog-js";
-import { PostHogProvider } from "posthog-js/react";
-import type { AppProps } from "next/app";
-
-if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host:
-      process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
-    person_profiles: "identified_only",
-    session_recording: {
-      recordCrossOriginIframes: true,
-    },
-  });
-}
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PostHogProvider client={posthog}>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Component {...pageProps} />
-    </PostHogProvider>
+    </ThemeProvider>
   );
 }
